d3985829e3b5d1f7736dc6ed49880d61b4650054
Jackson Louws <126889389+jkl77@users.noreply.github.com>
2023-03-27 13:28:13 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ pageName = ""
 
 app.config['SECRET_KEY'] = 'AF92AF84692AE0661B1CE20B'
 
-queensMessages = [{'title': 'Gord-Brock',
-             'content': 'Overall pretty mid 5/10'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'},
-            {'title': 'Message Three',
-             'content': 'Message Three Content'},
+queensResMessages = [{'title': 'Gord-Brock',
+             'content': 'Overall pretty mid 5/10'} 
+            ]
+
+queensCourseMessages = [{'title': 'COMM151',
+             'content': 'Not a fun class'}
+            ]
+
+queensFoodMessages = [{'title': 'Leonard Dining Hall',
+             'content': "Don't eat the eggs!!"}
+            ]
+
+queensSocialMessages = [{'title': 'Homecoming',
+             'content': "Best HOCO is Canada by far 10/10"}
             ]
 
 westernMessages = [{'title': 'Party Life',
@@ -34,7 +42,6 @@ def index():
      elif request.method == 'GET':
          return render_template('index.html', appName=appName)
      return render_template('index.html')
-
 
 @app.route('/queensHP', methods=('GET', 'POST'))
 def queensHP():
@@ -62,9 +69,60 @@ def queensRes():
         elif not content:
             flash('Content is empty!')
         else:
-            queensMessages.append({'title': title, 'content': content})
+            queensResMessages.append({'title': title, 'content': content})
             return redirect(url_for('queensRes'))
-    return render_template('queensRes.html', appName=appName, queensMessages=queensMessages)
+    return render_template('queensRes.html', appName=appName, queensResMessages=queensResMessages)
+
+@app.route('/queensCourse', methods=('GET', 'POST'))
+def queensCourse():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title and not content:
+            flash('Title and content are empty!')
+        elif not title:
+            flash('Title is empty!')
+        elif not content:
+            flash('Content is empty!')
+        else:
+            queensCourseMessages.append({'title': title, 'content': content})
+            return redirect(url_for('queensCourse'))
+    return render_template('queensCourse.html', appName=appName, queensCourseMessages=queensCourseMessages)
+
+@app.route('/queensFood', methods=('GET', 'POST'))
+def queensFood():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title and not content:
+            flash('Title and content are empty!')
+        elif not title:
+            flash('Title is empty!')
+        elif not content:
+            flash('Content is empty!')
+        else:
+            queensFoodMessages.append({'title': title, 'content': content})
+            return redirect(url_for('queensFood'))
+    return render_template('queensFood.html', appName=appName, queensFoodMessages=queensFoodMessages)
+
+@app.route('/queensSocial', methods=('GET', 'POST'))
+def queensSocial():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title and not content:
+            flash('Title and content are empty!')
+        elif not title:
+            flash('Title is empty!')
+        elif not content:
+            flash('Content is empty!')
+        else:
+            queensSocialMessages.append({'title': title, 'content': content})
+            return redirect(url_for('queensSocial'))
+    return render_template('queensSocial.html', appName=appName, queensSocialMessages=queensSocialMessages)
 
 @app.route('/western', methods=('GET', 'POST'))
 def western():
